0f245dd90441617bc21c38a876ab9ce5d905877a
Mohd Hafiz <xmhafiz@gmail.com>
2021-08-04 15:39:19 +0800

    init project

---
+++ SwiftUIListExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIListExamples
+//
+//  Created by Hafiz on 04/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIListExamples/SwiftUIListExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListExamplesApp.swift
+//  SwiftUIListExamples
+//
+//  Created by Hafiz on 04/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
